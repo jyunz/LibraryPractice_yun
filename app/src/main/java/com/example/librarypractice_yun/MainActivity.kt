f9498139e30dc5766155b8d2a8cf6d53a8deb0c0
Roy <J.jyun0525@gmail.com>
2022-03-15 10:40:54 +0900

    TedPermission 라이브러리 설치 + 권한 여부에 따른 행동방침 작성

--- app/src/main/java/com/example/librarypractice_yun/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_yun/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.librarypractice_yun
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +32,27 @@ class MainActivity : AppCompatActivity() {
         callBtn.setOnClickListener {
 
 //            라이브러리 활용 -> 전화 권한 확인 -> 실제 전화 연결
+
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+//                    권한승인 => 실제로 전화 연결 진행
+
+                    val myUri = Uri.parse("tel: 010-3333-3333")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+//                    권한 거절시 -> 토스트로 권한이 없어서 전화연결 실패 했다는 메세지 띄움
+                    Toast.makeText(this@MainActivity,"권한이 없어서 전화 연결에 실패 했습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
 
         }
     }
